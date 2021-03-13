22200a796fb62f6bd6e2ac2f6cc34c46476beea5
leoyunxi <leoyunxi@gmail.com>
2021-03-13 17:42:37 +0900

    init project

---
+++ src/main/kotlin/_01_Introduction/Hello.kt
@@ -0,0 +1,13 @@
+fun main() {
+    runBlock {
+        List(1000) {
+            println("HelloWorld")
+        }
+    }
+}
+
+fun runBlock(block: ()-> Unit){
+    val start = System.currentTimeMillis()
+    block()
+    println(System.currentTimeMillis() - start)
+}
